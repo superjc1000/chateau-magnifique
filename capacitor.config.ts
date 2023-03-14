b52d6bac45af8cf016db0c5060f7d459a1216323
superjc1000 <121019849+superjc1000@users.noreply.github.com>
2023-03-14 11:41:52 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'chateau magnifique',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
